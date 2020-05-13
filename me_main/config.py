e2fe391e3c0fd569f6109dc0a6761e3d39e06d72
yingjie <yingjie_qiao@outlook.com>
2020-05-13 15:27:23 +0800

    taken out secret info

--- me_main/config.py
+++ me_main/config.py
@@ -6,5 +6,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'iloveinit1'
-    MAIL_PASSWORD = 'q12wE34R'
+    MAIL_USERNAME = 'not going to show you this'
+    MAIL_PASSWORD = 'neither is this'
