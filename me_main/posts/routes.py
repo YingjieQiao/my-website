2d2d75d9e69f4a3a916f40d7782b4986ab890862
yingjie <yingjie_qiao@outlook.com>
2020-05-14 22:22:27 +0800

    small fix

--- me_main/posts/routes.py
+++ me_main/posts/routes.py
@@ -66,6 +66,7 @@ def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
+    db.session.delete(post.comments)
     db.session.delete(post)
     db.session.commit()
     flash("Your post has been deleted.", "success")
